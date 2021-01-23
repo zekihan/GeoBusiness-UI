4a0badac599c4e46f37334014594719718bb940e
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2021-01-23 22:26:20 +0300

    auth0 added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,75 @@
-import React from "react";
-import { Button, StyleSheet, View } from "react-native";
+import * as AuthSession from 'expo-auth-session';
+import jwtDecode from 'jwt-decode';
+import * as React from 'react';
+import { Alert, Button, Platform, StyleSheet, Text, View } from 'react-native';
+
+const auth0ClientId = "0RkrbD08xD6Bi1DuGrWPKSsDdlpiF1VU";
+const authorizationEndpoint = "https://geobusiness.eu.auth0.com/authorize";
+
+const useProxy = Platform.select({ web: false, default: true });
+const redirectUri = AuthSession.makeRedirectUri({ useProxy });
 
 export default function Main({ navigation }) {
   const onclick = () => {
     navigation.openDrawer();
   };
+  const [name, setName] = React.useState(null);
+
+  const [request, result, promptAsync] = AuthSession.useAuthRequest(
+    {
+      redirectUri,
+      clientId: auth0ClientId,
+      // id_token will return a JWT token
+      responseType: 'id_token',
+      // retrieve the user's profile
+      scopes: ['openid', 'profile'],
+      extraParams: {
+        // ideally, this will be a random value
+        nonce: 'nonce',
+      },
+    },
+    { authorizationEndpoint }
+  );
+
+  // Retrieve the redirect URL, add this to the callback URL list
+  // of your Auth0 application.
+  console.log(`Redirect URL: ${redirectUri}`);
+
+  React.useEffect(() => {
+    if (result) {
+      if (result.error) {
+        Alert.alert(
+          'Authentication error',
+          result.params.error_description || 'something went wrong'
+        );
+        return;
+      }
+      if (result.type === 'success') {
+        // Retrieve the JWT token and decode it
+        const jwtToken = result.params.id_token;
+        const decoded = jwtDecode(jwtToken);
+
+        const { name } = decoded;
+        setName(name);
+      }
+    }
+  }, [result]);
+
   return (
     <View style={styles.container}>
-      <Button onPress={onclick} title="Learn More" color="#841584" />
+      {name ? (
+        <>
+          <Text style={styles.title}>You are logged in, {name}!</Text>
+          <Button onPress={onclick} title="Learn More" color="#841584" />
+          <Button title="Log out" onPress={() => setName(null)} />
+        </>
+      ) : (
+          <Button
+            disabled={!request}
+            title="Log in with Auth0"
+            onPress={() => promptAsync({ useProxy })}
+          />
+        )}
     </View>
   );
 }
