32c0b37f11d4deaa858fe602776a8a94b1cc307c
Zekihan AZMAN <zekihanazman99@gmail.com>
2021-03-13 13:50:50 +0300

    webfix map

--- webFix.bat
+++ webFix.bat
@@ -1,4 +1,7 @@
 @echo off
 echo 'Fixing ViewPropTypes issues'
 REM Fix ViewPropTypes issues
+ECHO export const ColorPropType = { style: null };>>"./node_modules/react-native-web/dist/index.js"
+ECHO export const EdgeInsetsPropType = { style: null };>>"./node_modules/react-native-web/dist/index.js"
+ECHO export const PointPropType = { style: null };>>"./node_modules/react-native-web/dist/index.js"
 ECHO export const ViewPropTypes = { style: null };>>"./node_modules/react-native-web/dist/index.js"
